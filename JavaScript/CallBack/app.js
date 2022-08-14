29929e6790dcf99af78da8193e29bd30f8a956bd
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-14 20:36:17 +0530

    Async JS 14Aug22

---
+++ JavaScript/CallBack/app.js
@@ -0,0 +1,33 @@
+
+//document.body.style.backgroundColor = 'orange';
+
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'lightgreen';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'skyblue';
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay)
+}
+delayedColorChange('olive', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('teal', 1000);
+        })
+    })
+});
