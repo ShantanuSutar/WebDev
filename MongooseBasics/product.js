9cec6b156350549025c9ba99004c5aa42aa07755
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-23 22:32:07 +0530

    Mongodb and Mongoose Intro 23Aug22

---
+++ MongooseBasics/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connection open")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR")
+        console.log(err)
+    })
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product({
+    name: 'Mountain Bike', price: 599
+})
+bike.save()
+    .then(data => {
+        console.log("IT WORKED!")
+        console.log(data);
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!")
+        console.log(err)
+    })
