99f031b4f37465153e595e40437df5aca908436b
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-26 01:31:31 +0530

    Defining An Async Utility

--- AsyncErrors/index.js
+++ AsyncErrors/index.js
@@ -25,27 +25,32 @@ app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
-app.get('/products', async (req, res, next) => {
-    try {
-        const { category } = req.query;
-        if (category) {
-            const products = await Product.find({ category })
-            res.render('products/index', { products, category })
-        } else {
-            const products = await Product.find({})
-            res.render('products/index', { products, category: 'All' })
-        }
-    } catch (e) {
-        next(e);
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
     }
-})
+}
+
+
+app.get('/products', wrapAsync(async (req, res, next) => {
+
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+
+}))
 
 app.get('/products/new', (req, res) => {
     // throw new AppError('NOT ALLOWED', 401)
     res.render('products/new', { categories })
 })
 
-app.post('/products', async (req, res, next) => {
+app.post('/products', wrapAsync(async (req, res, next) => {
     try {
         const newProduct = new Product(req.body);
         await newProduct.save();
@@ -53,43 +58,37 @@ app.post('/products', async (req, res, next) => {
     } catch (e) {
         next(e);
     }
-})
+}))
 
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id)
-        if (!product) {
-            throw new AppError('Product Not Found', 404);
-        }
-        res.render('products/show', { product })
-    } catch (e) {
-        next(e);
-    }
-})
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        if (!product) {
-            throw next(new AppError('Product Not Found', 404));
-        }
-        res.render('products/edit', { product, categories })
-    } catch (e) {
-        next(e);
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    if (!product) {
+        throw new AppError('Product Not Found', 404);
     }
-})
+    res.render('products/show', { product })
 
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-        res.redirect(`/products/${product._id}`);
-    } catch (e) {
-        next(e);
+}))
+
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        throw next(new AppError('Product Not Found', 404));
     }
-})
+    res.render('products/edit', { product, categories })
+
+}))
+
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+
+}))
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
