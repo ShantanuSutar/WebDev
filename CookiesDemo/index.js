65883f0067afbb35d5333b296970bab29ccdd20e
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-31 01:14:53 +0530

    Signing Cookies

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisismysecret'));
+
 app.get('/greet', (req, res) => {
-    res.send("HEY THERE!")
+    const { name = 'No-name' } = req.cookies;
+    res.send(`HEY THERE, ${name}`)
 })
 
 app.get('/setname', (req, res) => {
-    res.cookie('name', 'stevie chicks')
+    res.cookie('animal', 'harlequin shrimp')
+    res.cookie('name', 'Shantanu')
     res.send('OK SENT YOU A COOKIE!!!')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('OK SIGNED YOUR FRUIT COOKIE!')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
