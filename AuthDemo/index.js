defb04c0d8ed311e5eb1c9520130d1a1a1af6a0d
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-01 21:29:43 +0530

    Auth Demo: Login

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -38,6 +38,21 @@ app.post('/register', async (req, res) => {
     res.redirect('/');
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (validPassword) {
+        res.send("YAY WELCOME!")
+    } else {
+        res.send('TRY AGAIN')
+    }
+})
+
 app.get('/secret', (req, res) => {
     res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
 })
