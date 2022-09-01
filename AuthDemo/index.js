c06c123af6204d48f345f976cd65c4f8f80408ea
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-01 21:43:28 +0530

    Auth Demo: Staying Logged In With Session

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user')
 const mongoose = require("mongoose")
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'notagoodsecret' }))
 
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE')
@@ -35,6 +37,7 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 })
 
@@ -47,13 +50,17 @@ app.post('/login', async (req, res) => {
     const user = await User.findOne({ username });
     const validPassword = await bcrypt.compare(password, user.password)
     if (validPassword) {
-        res.send("YAY WELCOME!")
+        req.session.user_id = user._id;
+        res.redirect('/secret')
     } else {
-        res.send('TRY AGAIN')
+        res.redirect('/login')
     }
 })
 
 app.get('/secret', (req, res) => {
+    if (!req.session.user_id) {
+        res.redirect('/login')
+    }
     res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
 })
 
