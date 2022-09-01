83228d882151502570e0a647352acceda060402a
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-01 19:21:54 +0530

    Auth Demo: Registering

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user')
+const mongoose = require("mongoose")
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("mongo connection open")
+    })
+    .catch(err => {
+        console.log("OH NO mongo connecion ERROR")
+        console.log(err)
+    })
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
 
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
--- AuthDemo/models/user.js
+++ AuthDemo/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    usename: {
+    username: {
         type: String,
         required: [true, 'Username cannot be blank']
     },
