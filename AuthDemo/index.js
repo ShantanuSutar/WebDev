cb56686c9c53c0b6c012989672ab8c3e4d6ed78b
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-01 21:57:39 +0530

    Auth Demo: Logout

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -57,11 +57,17 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login')
+})
+
 app.get('/secret', (req, res) => {
     if (!req.session.user_id) {
-        res.redirect('/login')
+        return res.redirect('/login')
     }
-    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
+    res.render('secret')
 })
 
 app.listen(3000, () => {
